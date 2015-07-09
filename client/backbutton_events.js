62a48387e345a95deb2d254e435805c0c5dc51ca
Scott <zhonglijunyi@163.com>
2015-07-09 18:20:10 +0800

    if press backbutton within other page, just go index page

--- client/backbutton_events.js
+++ client/backbutton_events.js
@@ -1,9 +1,9 @@
 if (Meteor.isCordova) {
   var onBackButtonPressed = function(event) {
     console.log("on back-button pressed");
-    for (var key in event) {
-      console.log(key+": "+event[key]);
-    }
+    // for (var key in event) {
+    //   console.log(key+": "+event[key]);
+    // }
     if (IonActionSheet.isShown) {
       IonActionSheet.isShown = false;
       IonActionSheet.close();
@@ -13,10 +13,17 @@ if (Meteor.isCordova) {
     var curRouterName = curRouter.route.getName();
     console.log("curRouterName is: "+curRouterName);
     if (curRouterName==="index") {
-      if (window.confirm('Are you sure to exit app!')) {
-        console.log('Exit app!');
-        navigator.app && navigator.app.exitApp && navigator.app.exitApp();
-      }
+      IonPopup.confirm({
+        title: 'Exit hint?',
+        template: 'Are you sure to exit app!?',
+        onOk: function() {
+          console.log('Exit app!');
+          navigator.app && navigator.app.exitApp && navigator.app.exitApp();
+        },
+        onCancel: function() {}
+      });
+    } else {
+      Router.go("index");
     }
   };
   document.addEventListener("backbutton", onBackButtonPressed, false);
